dcb815094ecd136c96c4069833fcd67f8599e792
ntcaston <ntcaston@gmail.com>
2013-07-25 22:16:06 +1200

    added item

---
+++ game/highlow/item.js
@@ -0,0 +1,11 @@
+goog.provide('plethora.game.HighLow.Item');
+
+
+goog.require('plethora.game.Item');
+
+
+plethora.game.HighLow.Item = function() {
+  goog.base(this);
+
+};
+goog.inherits(plethora.game.HighLow.Item, plethora.game.Item);
---
+++ game/item.js
@@ -0,0 +1,11 @@
+goog.provide('plethora.game.Item');
+
+
+goog.require('plethora.model.Item');
+
+
+
+plethora.game.Item= function() {
+  goog.base(this);
+};
+goog.inherits(plethora.game.Item, plethora.model.Item);
